012e9b6b8f72756e13eac37e63298e3799b91e2d
Nico Watine <contact@studioncreations.com>
2015-08-26 15:15:09 -0500

    Initial Commit

---
+++ js/src/skrollr-init.js
@@ -0,0 +1,6 @@
+if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+        forceHeight: true,
+        smoothScrolling: false
+    });
+}
