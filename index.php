3b3063c9b6c8c385441c7448571906126814cca1
Nico Watine <contact@studioncreations.com>
2015-08-30 10:12:55 -0500

    Edits to jquery1 reference

--- index.php
+++ index.php
@@ -104,7 +104,8 @@
 
 </div>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
 
 
